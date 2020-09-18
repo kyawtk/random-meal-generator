20df5ae03dc95d7c0d3c30173901e91cf5c8b869
Kyaw Thant Khine <kyawthantkhaing@gmail.com>
2020-09-18 16:17:42 +0630

    initial commit , too much unnecessary code ,Hooks were not needed for this small app .gonna refactor in the next commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import Meal from "./components/Meal";
+import "./App.css";
 
 function App() {
+  let [mealname, setmealname] = useState("");
+  let [area, setarea] = useState("");
+  let [instructions, setinstructions] = useState("");
+  let [tags, settags] = useState("");
+  let [category, setcategory] = useState("");
+  let [ingredients, setingredients] = useState([]);
+  let [image ,setimage] = useState('')
+
+  const getmeal = () => {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then((response) => response.json())
+      .then((data) => {
+        let meal = data.meals[0];
+        const getingredients = [];
+        console.log(meal);
+        setmealname(meal.strMeal);
+        setarea(meal.strArea);
+        setinstructions(meal.strInstructions);
+        settags(meal.strTags);
+        setcategory(meal.strCategory);
+        setimage(meal.strMealThumb)
+
+        // Get all ingredients from the object. Up to 20
+        for (let i = 1; i <= 20; i++) {
+          if (meal[`strIngredient${i}`]) {
+            getingredients.push(
+              `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+            );
+          } else {
+            // Stop if no more ingredients
+            break;
+          }
+        }
+        setingredients(getingredients);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getmeal}></button>
+      <Meal
+        name={mealname}
+        tags={tags}
+        ingredients={ingredients}
+        area={area}
+        instructions={instructions}
+        category={category}
+        image = {image}
+      />
     </div>
   );
 }
---
+++ src/components/Meal.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Meal = (props) => {
+    let {name,tags,category,area,instrctions,ingredients,image} = props;
+    
+    return ( <div className="meal-wrapper">
+
+    </div> );
+}
+ 
+export default Meal;
